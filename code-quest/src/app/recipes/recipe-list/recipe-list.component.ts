6387e7353251f34d66e3c7f0629bfccad7655d5f
Maciej Kawęcki <skimiskim12@gmail.com>
2024-02-08 20:41:19 +0100

    Componets & databiding

--- code-quest/src/app/recipes/recipe-list/recipe-list.component.ts
+++ code-quest/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../_models/recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../../_models/recipe.model';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'Recipe 1',
@@ -14,8 +15,8 @@ export class RecipeListComponent {
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'
     ),
     new Recipe(
-      'Recipe 1',
-      'This is the first recipe',
+      'Recipe 2',
+      'This is the first recipe 2',
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'
     ),
   ];
@@ -23,4 +24,8 @@ export class RecipeListComponent {
   constructor() {}
 
   ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
--- code-quest/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ code-quest/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { Ingredient } from '../../_models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.css'
+  styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+  @Output() ingriedientAdded = new EventEmitter<Ingredient>();
 
+  onAddItem(event: Event) {
+    event.preventDefault();
+
+    const newIngredient = new Ingredient(
+      this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value
+    );
+
+    this.ingriedientAdded.emit(newIngredient);
+  }
 }
