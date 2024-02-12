3199e9db0f05baadf757e48307b468210b2723fa
Maciej Kawęcki <skimiskim12@gmail.com>
2024-02-12 19:03:26 +0100

    Services and dependency injection

---
+++ services-assignment-start/src/app/inactive-users/inactive-users.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { UsersService } from '../users.service';
+
+@Component({
+  selector: 'app-inactive-users',
+  templateUrl: './inactive-users.component.html',
+  styleUrls: ['./inactive-users.component.css'],
+})
+export class InactiveUsersComponent {
+  users: string[];
+
+  constructor(private usersService: UsersService) {}
+
+  onSetToActive(id: number) {
+    this.usersService.setUsetToActive(id);
+  }
+
+  ngOnInit(): void {
+    this.users = this.usersService.inactiveUsers;
+  }
+}
